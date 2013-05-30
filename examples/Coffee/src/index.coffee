115e31a5d4f93ea7af2791d2c87de1d9531b99df
Anand Patil <anand.prabhakar.patil@gmail.com>
2013-05-29 17:01:44 -0700

    Improved coffeescript chunker

--- examples/Coffee/src/index.coffee
+++ examples/Coffee/src/index.coffee
@@ -1,6 +1,48 @@
 cp = require 'child_process'
 path = require 'path'
 _ = require 'underscore'
+marked = require 'marked'
+
+chunkBlockComments = (code) ->
+  splits = code.split /(###)/
+  chunks = []
+  comment = false
+  for split in splits
+    if split == '###'
+      comment = not comment
+    else if comment
+      chunks.push ['blockComment', split]
+    else
+      chunks.push ['unknown', split]
+  chunks
+
+chunkLineComments = (code) ->
+  lines = code.split '\n'
+  chunks = []
+  curChunk = ""
+  comment = false
+  for line in lines
+    trLine = line.trim()
+    if trLine[0] == '#'
+      if comment
+        curChunk += '\n' + line
+      else
+        if curChunk.length > 0 then chunks.push ['code', curChunk.trim()]
+        curChunk = line
+        comment = true
+    else
+      if comment
+        if curChunk.length > 0 then chunks.push ['comment', curChunk]
+        curChunk = line
+        comment = false
+      else
+        curChunk += '\n' + line
+  if curChunk.length > 0
+    if comment
+      chunks.push ['comment', curChunk]
+    else
+      chunks.push ['code', curChunk.trim()]
+  chunks
 
 module.exports = (dashboard) ->
   
@@ -29,13 +71,16 @@ module.exports = (dashboard) ->
   # can be sent in.
   dashboard.execute = (code, next) ->
     # If the code is a comment, we report it to the dashboard without
-    # communicating with the child process at all. If the comments were
-    # known to be in markdown format, the comment character '#' could be
-    # stripped and the comment text could be passed to dashboard.markdown.
+    # communicating with the child process at all.
     if code[0] == 'comment'
       dashboard.comment(code[1])
       # The dashboard is now ready to take the next code chunk.
       dashboard.prompt('coffee> ')
+      next()
+    # If the code is a block comment, we assume that it's 
+    else if code[0] == 'blockComment'
+      dashboard.markdown marked code[1]
+      dashboard.prompt('coffee>')
       next()
     else
       code = code[1]  
@@ -62,57 +107,15 @@ module.exports = (dashboard) ->
   # It doesn't split the code up into statements, which would make for nicer
   # looking dashboards.
   dashboard.chunk = (code) ->
-    chunks = []
-    buffer = ""
-    lines = code.split("\n")
-    inBlockComment = false
-    lastWasComment = false
-    for line in lines
-      trLine = line.trim()
-      if trLine.slice(0,3) == "###"
-        if inBlockComment
-          buffer += line
-          chunks.push ['comment', buffer.trim()]
-          buffer = ""
-          inBlockComment = false  
-        else
-          if buffer.trim() != ""
-            if lastWasComment
-              chunks.push ['comment', buffer.trim()]
-            else
-              chunks.push ['code', buffer.trim()]
-            buffer = ""
-          buffer += line + "\n"
-          inBlockComment = true
-      else if line[0] == "#" and not inBlockComment
-        if not lastWasComment and buffer.trim() != ""
-            chunks.push ['code', buffer.trim()]
-            buffer = ""
-        buffer += line + "\n"
-        lastWasComment = true
+    chunks = chunkBlockComments code
+    newChunks = []
+    for chunk in chunks
+      if chunk[0] == 'unknown'
+        newChunks.push.apply newChunks, chunkLineComments chunk[1]
       else
-        if inBlockComment
-          buffer += line + "\n"
-          continue
-        else if lastWasComment
-          chunks.push ['comment', buffer.trim()]
-          buffer = ""
-          lastWasComment = false
-        if line.trim().length == 0 or line[0] == " " or (line.slice(0,4) == "else" and line.trim().length == 4) or line.slice(0,4) == "else "
-          buffer += line + "\n"
-        else
-          if buffer.trim() != ""
-            chunks.push ['code', buffer.trim()]
-            buffer = ""
-          buffer = line + "\n"
-    if buffer
-      if lastWasComment
-        chunks.push ['comment', buffer.trim()]
-      else if inBlockComment
-        chunks.push ['comment', buffer.trim()]
-      else if buffer.trim() != ""
-        chunks.push ['code', buffer.trim()]
-    chunks
+        newChunks.push chunk
+    console.log JSON.stringify newChunks
+    newChunks
 
   # We report that the dashboard is ready to start taking input.
   dashboard.prompt('coffee> ')
--- examples/JS/lib/index.js
+++ examples/JS/lib/index.js
@@ -6,12 +6,19 @@ module.exports = function(dashboard) {
   dashboard.worker = cp.fork(path.join(__dirname, 'child'), [], {
     silent: true
   });
+
+  // We capture all output of the dashboard and report it as text. Note
+  // that this will catch calls to console.log and such, not the results
+  // of expressions that we evaluate.
   dashboard.worker.stdout.on('data', function(data) {
     return dashboard.text(data);
   });
   dashboard.worker.stderr.on('data', function(data) {
     return dashboard.text(data);
   });
+
+  // A very simple autocomplete function that just matches against
+  // the globals.
   dashboard.complete = function(substr) {
     var names,
       _this = this;
@@ -20,18 +27,33 @@ module.exports = function(dashboard) {
       return x.indexOf(substr) === 0;
     });
   };
+
+  // This interrupt function just sends the SIGINT signal to the worker
+  // process, but many other behaviors are possible.
   dashboard.interrupt = function() {
     return dashboard.worker.kill('SIGINT');
   };
+
+  // This function is responsible for sending code to the worker process
+  // for execution, returning any results to the dashboard, and notifying
+  // the dashboard when the computation has stopped and the next command
+  // can be sent in.
   dashboard.execute = function(code, next) {
     if (code[0] === 'comment') {
+      // If the code is a comment, we report it to the dashboard without
+      // communicating with the child process at all. If the comments were
+      // known to be in markdown format, the comment character '#' could be
+      // stripped and the comment text could be passed to dashboard.markdown.
       dashboard.comment(code[1]);
+      // The dashboard is now ready to take the next code chunk.
       dashboard.ready();
       next();
     } else {
       code = code[1];
       dashboard.code(code, 'javascript');
       dashboard.worker.send(code);
+      // The next message we get from the dashboard will be the result of 
+      // executing the code.
       return dashboard.worker.once('message', function(m) {
         switch (m.type) {
           case 'result':
@@ -48,77 +70,85 @@ module.exports = function(dashboard) {
           case 'html':
             dashboard.html(m.value);
         }
+        // Whether the code returned a result or caused an error, the dashboard
+        // is now ready to take the next code chunk.
         dashboard.ready();
         next();
       });
     }
   };
+
+  // A very simple chunker that splits code up into comments and actual code.
+  // It doesn't split the code up into statements, which would make for nicer
+  // looking dashboards.
   dashboard.chunk = function(code) {
     return [['code', code]];
-    // var buffer, chunks, inBlockComment, lastWasComment, line, lines, trLine, _i, _len;
-    // chunks = [];
-    // buffer = "";
-    // lines = code.split("\n");
-    // inBlockComment = false;
-    // lastWasComment = false;
-    // for (_i = 0, _len = lines.length; _i < _len; _i++) {
-    //   line = lines[_i];
-    //   trLine = line.trim();
-    //   if (trLine.slice(0, 3) === "###") {
-    //     if (inBlockComment) {
-    //       buffer += line;
-    //       chunks.push(['comment', buffer.trim()]);
-    //       buffer = "";
-    //       inBlockComment = false;
-    //     } else {
-    //       if (buffer.trim() !== "") {
-    //         if (lastWasComment) {
-    //           chunks.push(['comment', buffer.trim()]);
-    //         } else {
-    //           chunks.push(['code', buffer.trim()]);
-    //         }
-    //         buffer = "";
-    //       }
-    //       buffer += line + "\n";
-    //       inBlockComment = true;
-    //     }
-    //   } else if (line[0] === "#" && !inBlockComment) {
-    //     if (!lastWasComment && buffer.trim() !== "") {
-    //       chunks.push(['code', buffer.trim()]);
-    //       buffer = "";
-    //     }
-    //     buffer += line + "\n";
-    //     lastWasComment = true;
-    //   } else {
-    //     if (inBlockComment) {
-    //       buffer += line + "\n";
-    //       continue;
-    //     } else if (lastWasComment) {
-    //       chunks.push(['comment', buffer.trim()]);
-    //       buffer = "";
-    //       lastWasComment = false;
-    //     }
-    //     if (line.trim().length === 0 || line[0] === " " || (line.slice(0, 4) === "else" && line.trim().length === 4) || line.slice(0, 4) === "else ") {
-    //       buffer += line + "\n";
-    //     } else {
-    //       if (buffer.trim() !== "") {
-    //         chunks.push(['code', buffer.trim()]);
-    //         buffer = "";
-    //       }
-    //       buffer = line + "\n";
-    //     }
-    //   }
-    // }
-    // if (buffer) {
-    //   if (lastWasComment) {
-    //     chunks.push(['comment', buffer.trim()]);
-    //   } else if (inBlockComment) {
-    //     chunks.push(['comment', buffer.trim()]);
-    //   } else if (buffer.trim() !== "") {
-    //     chunks.push(['code', buffer.trim()]);
-    //   }
-    // }
-    // return chunks;
+    var buffer, chunks, inBlockComment, lastWasComment, line, lines, trLine, _i, _len;
+    chunks = [];
+    buffer = "";
+    lines = code.split("\n");
+    inBlockComment = false;
+    lastWasComment = false;
+    for (_i = 0, _len = lines.length; _i < _len; _i++) {
+      line = lines[_i];
+      trLine = line.trim();
+      if (trLine.slice(0, 3) === "*/") {
+        buffer += line;        
+        if (inBlockComment) {
+          chunks.push(['comment', buffer.trim()]);
+          buffer = "";
+          inBlockComment = false;
+        }
+      }
+      else {
+        if (buffer.trim() !== "") {
+          if (lastWasComment) {
+            chunks.push(['comment', buffer.trim()]);
+          } else {
+            chunks.push(['code', buffer.trim()]);
+          }
+          buffer = "";
+        }
+        buffer += line + "\n";
+        inBlockComment = true;
+      }
+      } else if (line[0] === "#" && !inBlockComment) {
+        if (!lastWasComment && buffer.trim() !== "") {
+          chunks.push(['code', buffer.trim()]);
+          buffer = "";
+        }
+        buffer += line + "\n";
+        lastWasComment = true;
+      } else {
+        if (inBlockComment) {
+          buffer += line + "\n";
+          continue;
+        } else if (lastWasComment) {
+          chunks.push(['comment', buffer.trim()]);
+          buffer = "";
+          lastWasComment = false;
+        }
+        if (line.trim().length === 0 || line[0] === " " || (line.slice(0, 4) === "else" && line.trim().length === 4) || line.slice(0, 4) === "else ") {
+          buffer += line + "\n";
+        } else {
+          if (buffer.trim() !== "") {
+            chunks.push(['code', buffer.trim()]);
+            buffer = "";
+          }
+          buffer = line + "\n";
+        }
+      }
+    }
+    if (buffer) {
+      if (lastWasComment) {
+        chunks.push(['comment', buffer.trim()]);
+      } else if (inBlockComment) {
+        chunks.push(['comment', buffer.trim()]);
+      } else if (buffer.trim() !== "") {
+        chunks.push(['code', buffer.trim()]);
+      }
+    }
+    return chunks;
   };
   return dashboard.ready();
 };
